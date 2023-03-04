02b789a9536a64a9d3b680f57bc1880992aa4717
pcher93 <109242086+pcher93@users.noreply.github.com>
2023-03-04 15:45:28 -0600

    Add jquery & interactions

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log ("Your index.js file is loaded correctly!");
+$("nav a").on({
+    hover: function(){
+      $(this).css("background-color", "yellow");
+    } 
+    
+  });
+
+  $("nav a").ready(function() {
+    $('leftbutton').click(function() {
+      window.location.href = "https://drive.google.com/file/d/1ZD_du3jUAtHHFFzkJ_Xif80cz67BFg6n/view";
+    });
+  });
