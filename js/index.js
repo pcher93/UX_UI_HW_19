aaaead0de131399f3057c67e34b364a7c2f3de02
pcher93 <109242086+pcher93@users.noreply.github.com>
2023-03-07 00:06:14 -0600

    update hover, project img, selected nav
    iterations based off peer feedback

--- js/index.js
+++ js/index.js
@@ -10,3 +10,11 @@ $("nav a").on({
       window.location.href = "https://drive.google.com/file/d/1ZD_du3jUAtHHFFzkJ_Xif80cz67BFg6n/view";
     });
   });
+
+  $(document).ready(function() {
+    $('nav a').click(function() {
+      $('nav a').removeClass('home'); // Remove the active class from all links
+      $(this).addClass('home'); // Add the active class to the clicked link
+    });
+  });
+  
